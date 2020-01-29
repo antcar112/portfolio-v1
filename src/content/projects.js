3e50a4db56e6a3aee5d488e30197ebf96b779227
Anthony Caron <antcar1992@gmail.com>
2020-01-29 09:38:46 -0800

    updated volt page description

--- src/content/projects.js
+++ src/content/projects.js
@@ -22,7 +22,7 @@ const projects = [
 			`Timeego was the result of a four-month project class, in which our team of three conceived, designed and developed a web application. We used an Agile design methodology to build this app. The final project was judged on good UX/UI, complete documentation, and overall functionality.`,
 			`The original pitch for this app was to build something that would allow students to log their time studying, and view their total study time in each course. This would help students track, manage and ultimately improve their study habits. I'm proud to say the finished product lives up to this original idea.`,
 			`The feature I'm most proud of in this app is the working stopwatch that can be used to track study times. It was built using object-oriented programming, and uses local storage to save it's state. This allows the time to persist across different page loads without the need for a true backend.`,
-			`When we started coding, we quickly realized we would have a lot of different JavaScript files. To improve our codes organization, we implemented a Model-View-Controller design pattern. This ultimately made the finished product much easier to navigate.`,
+			`When we started coding, we quickly realized we would have a lot of different JavaScript files. To improve our codes organization, we implemented a Model-View-Controller design pattern. This ultimately made the finished product much easier to navigate.`
 		],
 		features    : [
 			`A working stopwatch that is able to track times across different page loads.`,
@@ -30,8 +30,8 @@ const projects = [
 			`Displays study times in a graphical format.`,
 			`Developed using the Agile design methodology.`,
 			`Code organized using the MVC design pattern.`,
-			`Uses Google Firebase for a database and for authorization.`,
-		],
+			`Uses Google Firebase for a database and for authorization.`
+		]
 	},
 	{
 		title       : 'Cody Cameron',
@@ -42,46 +42,42 @@ const projects = [
 		image       : codyImg,
 		video       : codyVideo,
 		background  : '#A3D9FF',
-		githubLink  :
-			'https://github.com/antcar112/cody-cameron-portfolio',
-		hostedLink  :
-			'https://eloquent-wozniak-860d04.netlify.com/',
+		githubLink  : 'https://github.com/antcar112/cody-cameron-portfolio',
+		hostedLink  : 'https://eloquent-wozniak-860d04.netlify.com/',
 		devDesc     : [
 			`When one of my close friends asked me to build him a portfolio website, I jumped on the opportunity. I saw this as a great chance to learn something new while practicing my web development skills.`,
 			`My biggest challenge building this site was making it so Cody could manage his own content without any outside help. Originally, I planned on using the MERN stack to accomplish this. However, when I heard about Gatsby and how easy it makes it to dynamically render new pages, I opted to try that approach instead.`,
-			`Ultimately, the finished site is built using the JAM stack. Gatsby is used for the front-end while the Contentful headless CMS stores the sites content. GraphQL then queries Contentful to get content to display on the site. This setup means Cody can manage all his own content.`,
+			`Ultimately, the finished site is built using the JAM stack. Gatsby is used for the front-end while the Contentful headless CMS stores the sites content. GraphQL then queries Contentful to get content to display on the site. This setup means Cody can manage all his own content.`
 		],
 		features    : [
 			`Uses the Contentful headless CMS to allow Cody to manage his own content.`,
 			`Gatsby will automatically re-render pages as content on the CMS is changed.`,
-			`The updated site is then automatically redeployed on Netlify.`,
-		],
+			`The updated site is then automatically redeployed on Netlify.`
+		]
 	},
 	{
 		title       : 'Volt',
 		subtitle    : 'QDS20 Hackathon App',
 		description :
-			'A web application designed display complex mining data in a simple, graphical form. This project was made for the QDS20 Hackathon hosted at BCIT.',
+			'A web application designed to display complex mining data in a simple, graphical form. This project was made for the QDS20 Hackathon hosted at BCIT.',
 		link        : '/volt/',
 		image       : voltImage,
 		video       : voltVideo,
 		background  : '#7E6B8F',
-		githubLink  :
-			'https://github.com/antcar112/qds-20-hackathon',
-		hostedLink  :
-			'https://hardcore-dubinsky-9bf55c.netlify.com/',
+		githubLink  : 'https://github.com/antcar112/qds-20-hackathon',
+		hostedLink  : 'https://hardcore-dubinsky-9bf55c.netlify.com/',
 		devDesc     : [
 			`Volt is a web application made for the QDS20 Hackathon, hosted at BCIT in January 2020. It was built by a team of five students over a 27-hour period. The theme of the hackathon was data analytics.`,
 			`Volt's purpose is to display complex mining data in a simple, graphical form. This allows company executives to easily spot trends affecting the overall productivity of the company.`,
 			`The mining data was provided by the hackathon organizers in the form of JSON. We parsed, organized and analysed this data using the Python library Pandas.`,
-			`The web application is built using the MERN stack. React is used to build the front end and Node and Express are used on the backend. The parsed mining data is stored in a MongoDB database.`,
-			`My personal role in this project was building the MERN portion of the app as well as designing the layout.`,
+			`The web application is built using the MERN stack. React was used to build the front end and Node and Express are used for the backend of the app. The parsed mining data is stored in a MongoDB database.`,
+			`My personal role in this project was building the MERN portion of the app as well as designing the layout.`
 		],
 		features    : [
 			`Uses the MERN stack to build a fast, responsive full stack web application.`,
 			`Displays complex data in a simple, graphical form.`,
-			`Uses ApexCharts, a JavaScript library, to display dynamic charts.`,
-		],
+			`Uses ApexCharts, a JavaScript library, to display dynamic charts.`
+		]
 	},
 	{
 		title       : "Big General's",
@@ -92,19 +88,17 @@ const projects = [
 		image       : pizzaImg,
 		video       : pizzaVideo,
 		background  : '#F7E8A4 ',
-		githubLink  :
-			'https://github.com/antcar112/big-generals-pizza',
-		hostedLink  :
-			'https://happy-jepsen-721e54.netlify.com/index.html',
+		githubLink  : 'https://github.com/antcar112/big-generals-pizza',
+		hostedLink  : 'https://happy-jepsen-721e54.netlify.com/index.html',
 		devDesc     : [
 			`This site was a project for the first web development course I took. The goal of the project was to design a static website for a small business of our choice using HTML, CSS and some very basic JavaScript.`,
-			`The main focus of this project was making a well designed and attractive website. I settled on using a colorful design to catch the users eye. To keep the site simple to use, I tried to keep the amount of information on the screen to a minimum. I also used this project as an opportunity to use Sass for the first time. Doing this really increased the speed that I could write my CSS.`,
+			`The main focus of this project was making a well designed and attractive website. I settled on using a colorful design to catch the users eye. To keep the site simple to use, I tried to keep the amount of information on the screen to a minimum. I also used this project as an opportunity to use Sass for the first time. Doing this really increased the speed that I could write my CSS.`
 		],
 		features    : [
 			`A static HTML website focused on a clean design and ease of use.`,
-			`Uses Sass to speed up process of writing CSS.`,
-		],
-	},
+			`Uses Sass to speed up process of writing CSS.`
+		]
+	}
 ];
 
 export const timeego = projects[0];
