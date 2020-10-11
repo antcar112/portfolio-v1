87bfd8bae7b0c0f8d6b4d0a5271d04c8c013a41b
Anthony Caron <antcar1992@gmail.com>
2020-10-11 15:15:17 -0700

    moved buttonStyles

--- src/ui/components/button/Button.jsx
+++ src/ui/components/button/Button.jsx
@@ -1,7 +1,132 @@
-import { BtnOutlineStyles, BtnSolidStyles } from '@src/ui/components/button/buttonStyles'
+import { media } from '@src/utils'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 import React, { useContext } from 'react'
-import styled, { ThemeContext } from 'styled-components'
+import styled, { css, ThemeContext } from 'styled-components'
+
+const SharedBtnStyles = css`
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  text-decoration: none;
+  transition: ${({ theme }) => theme.hoverTransition}, border-color 300ms linear;
+  overflow: hidden;
+  width: 250px;
+  height: 60px;
+
+  &::after {
+    content: '';
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    bottom: 0;
+    left: 110%;
+    width: 120%;
+    transition: all 375ms;
+  }
+
+  &:hover::after,
+  &:active::after,
+  &:focus::after {
+    left: -10%;
+  }
+
+  & div {
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+    display: grid;
+    place-items: center center;
+    pointer-events: none;
+  }
+
+  & span {
+    display: inline-block;
+    font-size: 1.1em;
+    pointer-events: none;
+  }
+
+  & .btn_text--default {
+    transition: transform 500ms, opacity 250ms 120ms;
+    opacity: 1;
+  }
+
+  &:hover .btn_text--default,
+  &:active .btn_text--default,
+  &:focus .btn_text--default {
+    opacity: 0;
+    transform: translateX(-20%);
+    transition: transform 500ms, opacity 250ms;
+  }
+
+  & .btn_text--hidden {
+    position: absolute;
+    transition: right 500ms, opacity 250ms;
+    opacity: 0;
+    right: -20%;
+    top: 0;
+    z-index: 2;
+  }
+
+  &:hover .btn_text--hidden,
+  &:active .btn_text--hidden,
+  &:focus .btn_text--hidden {
+    right: 0;
+    opacity: 1;
+    transition: right 500ms, opacity 250ms 120ms;
+  }
+
+  ${media.down.sm} {
+    height: 50px;
+    width: 100%;
+
+    & span {
+      font-size: 1em;
+    }
+  }
+`
+
+const BtnSolidStyles = css`
+  ${SharedBtnStyles};
+  border: none;
+  background: ${({ theme }) => theme.color.primary};
+
+  &::after {
+    background: ${({ theme }) => theme.color.primaryDarkened};
+  }
+
+  & span {
+    color: white;
+  }
+`
+
+const BtnOutlineStyles = css`
+  ${SharedBtnStyles};
+  border: 4px solid ${({ theme }) => theme.color.primary};
+  background: none;
+
+  &:hover {
+    border-color: ${({ theme }) => theme.color.primaryDarkened};
+  }
+
+  &::after {
+    background: ${({ theme }) => theme.color.primaryDarkened};
+  }
+  & div {
+    width: 100%;
+  }
+
+  & .btn_text--default {
+    & span {
+      color: ${({ theme }) => theme.color.primary};
+    }
+  }
+
+  & .btn_text--hidden {
+    & span {
+      color: white;
+    }
+  }
+`
 
 const StyledButton = styled.button`
   ${({ outline }) => (outline ? BtnOutlineStyles : BtnSolidStyles)};
@@ -17,6 +142,7 @@ const StyledAniLink = styled(AniLink)`
 
 export const Button = ({ direction, handleClick, href, outline = false, text, to }) => {
   const themeContext = useContext(ThemeContext)
+
   if (to) {
     return (
       <StyledAniLink
